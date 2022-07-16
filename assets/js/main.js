d815851b963f164bc6d2124f5aa81c6d8a495a42
unknown <ajibolamubarak304@gmail.com>
2022-07-16 02:32:03 +0100

    working on the style section 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,13 @@
 const showModal = (openButton, modalContent) => {
   const openBtn = document.getElementById(openButton);
   const modalContainer = document.getElementById(modalContent);
+
+  if (openBtn && modalContainer) {
+    openBtn.addEventListener("click", () => {
+      modalContainer.classList.add("show-modal");
+    });
+  }
 };
+showModal("open-modal", "modal-container");
 
 /*=============== CLOSE MODAL ===============*/
